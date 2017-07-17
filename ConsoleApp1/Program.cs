32955e5f77ef60941c827edda1d0be1717d7eded
Fabian <fabian@astucemedia.com>
2017-07-17 13:40:43 -0400

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ServiceStack;
+using ServiceStack.Text;
+using TechStacks.ServiceModel;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new JsonServiceClient("http://techstacks.io/");
+            var response = client.Get(new GetAllTechnologies());
+
+            //Print contents of the first technology to the Console
+            response.Results[0].PrintDump();
+            Console.ReadLine();
+        }
+    }
+}
